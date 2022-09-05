651399ed88d8c9b44afb063ec79fbdcd86ce42f6
hamzasaadi <hamza1mail@gmail.com>
2022-09-05 01:41:15 +0100

    initial

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ListingController;
+
+
+Route::get('/', [ListingController::class, 'index']);
+
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
